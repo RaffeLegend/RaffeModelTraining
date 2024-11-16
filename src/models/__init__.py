55e5f8f6a9e3d182d4d7f2ff7ed264f3077a699a
RaffeLegend <oldgirafferiver@gmail.com>
2024-11-16 20:18:32 +0000

    [#1] fix: fix the import path

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
-from src.models.model_zoo.decoder.clip_models import CLIPModel
-from src.models.model_zoo.decoder.imagenet_models import ImagenetModel
-from models.model_zoo.encoder.classification.fc_layer import FCDecoder
+from src.models.model_zoo.encoder.clip_models import CLIPModel
+from src.models.model_zoo.encoder.imagenet_models import ImagenetModel
+from models.model_zoo.decoder.classification.fc_layer import FCDecoder
 
 from model_list import VALID_DECODER_NAMES, VALID_ENCODER_NAMES, CHANNELS
 
