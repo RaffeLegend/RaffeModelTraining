344603375272a76769f9b67adfb412888734b6c2
RaffeLegend <oldgirafferiver@gmail.com>
2024-11-16 20:25:10 +0000

    [#1] update: update the requirement and import path

--- src/models/model_zoo/encoder/imagenet_models.py
+++ src/models/model_zoo/encoder/imagenet_models.py
@@ -1,5 +1,5 @@
 from .resnet.resnet import resnet18, resnet34, resnet50, resnet101, resnet152
-from .vision_transformer import vit_b_16, vit_b_32, vit_l_16, vit_l_32
+from .vision_transformer.vision_transformer import vit_b_16, vit_b_32, vit_l_16, vit_l_32
 
 import torch.nn as nn 
 
