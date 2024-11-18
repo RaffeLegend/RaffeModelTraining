c856af76f16932b70e8dab552ce9b20b06109dc0
RaffeLegend <oldgirafferiver@gmail.com>
2024-11-18 09:21:26 +0000

    [#1] fix: fix the model saving issue

--- src/engine/base_trainer.py
+++ src/engine/base_trainer.py
@@ -2,6 +2,7 @@ import os
 import torch
 import torch.nn as nn
 from torch.optim import lr_scheduler
+from src.utils.tools import mkdirs
 
 
 class BaseModel(nn.Module):
@@ -13,7 +14,8 @@ class BaseModel(nn.Module):
         self.device = torch.device('cuda:{}'.format(opt.gpus[0])) if opt.gpus else torch.device('cpu')
 
     def save_networks(self, save_filename):
-        save_path = os.path.join(self.save_dir, self.opt.name, self.opt.model_name)
+        save_path = os.path.join(self.save_dir, self.opt.model_name)
+        mkdirs(save_path)
 
         # serialize model and optimizer to dict
         state_dict = {
@@ -22,7 +24,7 @@ class BaseModel(nn.Module):
             'total_steps' : self.total_steps,
         }
 
-        torch.save(state_dict, save_path)
+        torch.save(state_dict, os.path.join(save_path, save_filename))
 
 
     def eval(self):
