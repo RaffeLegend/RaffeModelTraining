f006d7577685d33b27f931a053bfb250885e9bb3
RaffeLegend <oldgirafferiver@gmail.com>
2024-11-20 21:42:10 +0000

    [#1] update: update the validate

--- src/validate.py
+++ src/validate.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from src.options import config_settings
 from src.data import create_dataloader
+from src.engine.trainer import Trainer
 from src.engine.validator import Validator
 from src.utils.tools import log_validation_metrics
 import torch
@@ -14,13 +15,15 @@ def evaluate_model():
     # Load the model
     opt = config_settings()
 
+    trainer = Trainer(opt.train)
     data_loader = create_dataloader(opt.test_data)
 
-    model = torch.load(opt.test.model_path)
-    model.eval()
-
     validator = Validator(opt.val)
-    validator.update(model, data_loader)
+    validator.update(trainer.model, data_loader)
+    net_state_dict = torch.load(opt.test.model_path, map_location='cuda:0')
+    validator.model.load_state_dict(net_state_dict['model'])
+
+    validator.model.eval()
 
     validator.forward()
     ap, r_acc, f_acc, acc = validator.evaluate()
